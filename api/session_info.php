f7937ce42856320a1d6d5d6f669e7497b15a3524
AlaeBendahmane <alaebendahmane1@gmail.com>
2026-03-18 14:04:04 +0000

    add Référence to auto update

--- api/session_info.php
+++ api/session_info.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-$idFiscal = $_SESSION['idFiscal'] ?? ""; //toprofile
-$idProx = $_SESSION['idProx'] ?? ""; //toprofile
-$nom = $_SESSION['nom'] ?? ""; //
-$prenom = $_SESSION['prenom'] ?? ""; //
-$email = $_SESSION['email'] ?? ""; //
-$ste = $_SESSION['ste'] ?? ""; //toprofile
-$campagne = $_SESSION['campagne'] ?? ""; //
-$role = $_SESSION['role'] ?? ""; //
-$profilePic = $_SESSION['profilePic'] ?? "";
-$token =  $_SESSION['token'] ?? "";
-$needReset = $_SESSION['needReset'] ?? 1;
+$idFiscal = &$_SESSION['idFiscal'] ?? ""; //toprofile
+$idProx = &$_SESSION['idProx'] ?? ""; //toprofile
+$nom = &$_SESSION['nom'] ?? ""; //
+$prenom = &$_SESSION['prenom'] ?? ""; //
+$email = &$_SESSION['email'] ?? ""; //
+$ste = &$_SESSION['ste'] ?? ""; //toprofile
+$campagne = &$_SESSION['campagne'] ?? ""; //
+$role = &$_SESSION['role'] ?? ""; //
+$profilePic = &$_SESSION['profilePic'] ?? "";
+$token =  &$_SESSION['token'] ?? "";
+$needReset = &$_SESSION['needReset'] ?? 1;
