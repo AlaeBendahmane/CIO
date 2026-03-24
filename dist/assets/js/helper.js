15e7f55470ed3ea12c32b8741ec244dd34571101
AlaeBendahmane <alaebendahmane1@gmail.com>
2026-03-24 16:50:30 +0100

    Add notification popup

--- dist/assets/js/helper.js
+++ dist/assets/js/helper.js
@@ -2,3 +2,34 @@ console.log = function () { };
 console.info = function () { };
 console.warn = function () { };
 console.error = function () { };
+
+function showTeamsNotification(title, message) {
+    if (!("Notification" in window)) {
+        console.log("This browser does not support desktop notification");
+        return;
+    }
+
+    if (Notification.permission === "granted") {
+        createNotification(title, message);
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                createNotification(title, message);
+            }
+        });
+    }
+}
+
+function createNotification(title, message) {
+    const options = {
+        body: message,
+        icon: './img/C.png',
+        silent: false,
+        requireInteraction: false
+    };
+
+    new Notification(title, options);
+}
+
+// showTeamsNotification("Pointage Update", 'log.message');
+//getnotifications
