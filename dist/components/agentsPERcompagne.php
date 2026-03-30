0f42dc6d03307258c4050f6af1387e65fd1cd868
AlaeBendahmane <alaebendahmane1@gmail.com>
2026-03-30 14:01:16 +0200

    remove tool tip in agents per compagnes

--- dist/components/agentsPERcompagne.php
+++ dist/components/agentsPERcompagne.php
@@ -4,11 +4,11 @@ include '../api/agentsPERcompagnes.php';
 <div class="card card-primary shadow-sm"><!--resizable-card-->
     <div class="card-header ">
         <h3 class="card-title">Utilisateurs par Campagnes</h3>
-        <div class="card-tools">
+        <!-- <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse">
                 <i class="fas fa-minus"></i>
             </button>
-        </div>
+        </div> -->
     </div>
     <div class="card-body">
         <div id="agentDonutChart" style="min-height: 250px;"></div>
