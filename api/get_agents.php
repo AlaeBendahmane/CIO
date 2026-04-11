b80c816962a5ad65891c3e1a242213973a7a3b7c
AlaeBendahmane <alaebendahmane1@gmail.com>
2026-04-11 20:01:33 +0200

    add from for notification in get_agents

--- api/get_agents.php
+++ api/get_agents.php
@@ -6,37 +6,54 @@ require_once 'helpers.php';
 session_start();
 isAuthQuery();
 isAdminQuery();
-
+$from = $_GET['from'] ?? null;
 try {
-    $stmt = $pdo->prepare("SELECT id, idFiscal, idProx, nom, prenom, email, ste, campagne ,role FROM agents WHERE isDeleted = 0");
-    $stmt->execute();
-    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if ($from == "notifications") {
+        $stmt = $pdo->prepare("SELECT id, idFiscal ,idProx, nom, prenom FROM agents WHERE isDeleted = 0");
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $users = array_map(function ($user) {
-        $user['nom'] = strtoupper($user['nom']);
-        $user['prenom'] = strtoupper($user['prenom']);
-        $user['ste'] = strtoupper($user['ste']);
-        $user['campagne'] = strtoupper($user['campagne']);
-        $_role = 'U';
-        if ($user['role'] == 'U') {
-            $_role = 'Agent';
-        } else if ($user['role'] == 'C') {
-            $_role = 'Coach';
-        } else if ($user['role'] == 'A') {
-            $_role = 'Administrateur';
-        } else if ($user['role'] == 'M') {
-            $_role = 'Mex';
-        } else {
-            $_role = 'Agent';
-        }
-        $user['role'] = $_role;
-        return $user;
-    }, $users);
+        $users = array_map(function ($user) {
+            $user['nom'] = strtoupper($user['nom']);
+            $user['prenom'] = strtoupper($user['prenom']);
+            return $user;
+        }, $users);
 
-    echo json_encode([
-        'status' => 'success',
-        'data' => $users
-    ]);
+        echo json_encode([
+            'status' => 'success',
+            'data' => $users
+        ]);
+    } else {
+        $stmt = $pdo->prepare("SELECT id, idFiscal, idProx, nom, prenom, email, ste, campagne ,role FROM agents WHERE isDeleted = 0");
+        $stmt->execute();
+        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $users = array_map(function ($user) {
+            $user['nom'] = strtoupper($user['nom']);
+            $user['prenom'] = strtoupper($user['prenom']);
+            $user['ste'] = strtoupper($user['ste']);
+            $user['campagne'] = strtoupper($user['campagne']);
+            $_role = 'U';
+            if ($user['role'] == 'U') {
+                $_role = 'Agent';
+            } else if ($user['role'] == 'C') {
+                $_role = 'Coach';
+            } else if ($user['role'] == 'A') {
+                $_role = 'Administrateur';
+            } else if ($user['role'] == 'M') {
+                $_role = 'Mex';
+            } else {
+                $_role = 'Agent';
+            }
+            $user['role'] = $_role;
+            return $user;
+        }, $users);
+
+        echo json_encode([
+            'status' => 'success',
+            'data' => $users
+        ]);
+    }
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode([
