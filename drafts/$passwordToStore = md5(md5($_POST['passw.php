a94e928cd4749802b72114f6000877432246d4d0
AlaeBendahmane <alaebendahmane1@gmail.com>
2026-04-16 09:22:48 +0200

    remove drafts

--- drafts/$passwordToStore = md5(md5($_POST['passw.php
+++
@@ -1,6 +0,0 @@
-$passwordToStore = md5(md5($_POST['password']));
-
-$sql = "INSERT INTO agents (idFiscal, nom, prenom, email, password) VALUES (?, ?, ?, ?, ?)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$idFiscal, $nom, $prenom, $email, $passwordToStore]);
-
